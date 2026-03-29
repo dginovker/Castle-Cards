3feaa9b33a3fe8e699af80b8822daf7381de6ab3
dginovker <danielginovker@gmail.com>
2026-03-29 00:31:57 -0700

    add credits scene

--- .ziva/snapshots/-6k-0Ml8z5NhIQiANhoYo/ja2_II5Hrp6UQp3lDxXjI/scripts__main_menu.gd
+++ .ziva/snapshots/-6k-0Ml8z5NhIQiANhoYo/ja2_II5Hrp6UQp3lDxXjI/scripts__main_menu.gd
@@ -3,28 +3,15 @@ extends Node2D
 
 @onready var play_button: Button = %PlayButton
 @onready var upgrades_button: Button = %UpgradesButton
-@onready var credits_button: Button = %CreditsButton
 
 func _ready() -> void:
     play_button.pressed.connect(_on_play_pressed)
     upgrades_button.pressed.connect(_on_upgrades_pressed)
-    credits_button.pressed.connect(_on_credits_pressed)
     
     play_button.mouse_entered.connect(_on_button_mouse_entered.bind(play_button))
     play_button.mouse_exited.connect(_on_button_mouse_exited.bind(play_button))
     upgrades_button.mouse_entered.connect(_on_button_mouse_entered.bind(upgrades_button))
     upgrades_button.mouse_exited.connect(_on_button_mouse_exited.bind(upgrades_button))
-    credits_button.mouse_entered.connect(_on_credits_hover.bind(true))
-    credits_button.mouse_exited.connect(_on_credits_hover.bind(false))
-
-func _on_credits_hover(is_hover: bool) -> void:
-    if is_hover:
-        credits_button.modulate = Color(0.8, 0.8, 1.0)
-    else:
-        credits_button.modulate = Color(1.0, 1.0, 1.0)
-
-func _on_credits_pressed() -> void:
-    get_tree().change_scene_to_file("res://scenes/credits.tscn")
     
 func _on_button_mouse_entered(button: Button) -> void:
     var tween = create_tween().set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
